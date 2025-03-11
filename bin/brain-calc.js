c0dfceebd189f899d82a945062b61c165a9faed0
Даниил <you@example.com>
2025-03-11 17:24:07 +0300

    calc revert

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,44 +1,39 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import askName from '../src/cli.js';
-import { checkCorrectAnswer, getRandomNumber } from '../src/index.js';
+import readlineSync from "readline-sync";
+import askName from "../src/cli.js";
+import { checkCorrectAnswer, getRandomNumber } from "../src/index.js";
+
 
 function brainCalc() {
   const name = askName();
-  console.log('What is the result of the expression?');
+  console.log("What is the result of the expression?");
   let i = 0;
   while (i < 3) {
     const number1 = getRandomNumber();
     const number2 = getRandomNumber();
-    const operators = ['+', '-', '*'];
-    const randomOperator = operators[Math.floor(Math.random() * operators.length)];
-
+    const operators = ["+", "-", "*"];
+    const randomOperator =
+      operators[Math.floor(Math.random() * operators.length)];
     console.log(`Question: ${number1} ${randomOperator} ${number2}`);
-    const answer = readlineSync.question('Your answer:').toLowerCase();
-    let correctAnswer = '';
-
+    const answer = readlineSync.question("Your answer:").toLowerCase();
+    let correctAnswer = "";
     switch (randomOperator) {
-      case '+':
+      case "+":
         correctAnswer = (number1 + number2).toString();
         break;
-      case '-':
+      case "-":
         correctAnswer = (number1 - number2).toString();
         break;
-      case '*':
+      case "*":
         correctAnswer = (number1 * number2).toString();
         break;
       default:
         throw new Error(`Unknown operator: ${randomOperator}`);
     }
-
     const check = checkCorrectAnswer(answer, correctAnswer, name);
-    if (check) {
-      i += 1;
-    } else {
-      return;
-    }
+    if (check) i += 1;
+    else return;
   }
   console.log(`Congratulations, ${name}!`);
 }
-
 brainCalc();
