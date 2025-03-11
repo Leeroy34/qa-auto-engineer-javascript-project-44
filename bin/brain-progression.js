0648dabbd5a1b9465ba43a287c6348071996c6bb
Даниил <you@example.com>
2025-03-11 14:42:03 +0300

    progression  was fixed

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -13,14 +13,17 @@ function brainProgression() {
   for (let i = 0; i < 3; i++) {
     const start = getRandomNumber(1, 50);
     const step = getRandomNumber(1, 10);
-    const length = getRandomNumber(5, 10);
+    let length = getRandomNumber(5, 10);
+
+    if (length < 5) length = 5;
 
     const progression = generateProgression(start, step, length);
-    const hiddenIndex = getRandomNumber(0, length - 1); // Скрываем случайный элемент
+    const hiddenIndex = getRandomNumber(0, length - 1); // Индекс скрытого элемента
     const correctAnswer = progression[hiddenIndex].toString();
 
     progression[hiddenIndex] = "..";
-    console.log(`Question: ${progression.join(" ")}`);
+
+    console.log(`Question: ${progression.join(" ")}`.trim());
 
     const answer = readlineSync.question("Your answer: ");
 
