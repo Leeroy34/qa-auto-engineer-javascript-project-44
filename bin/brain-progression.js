a7b4db3481b96cc1cd3032c4f6f0644a2905104e
Даниил <you@example.com>
2025-03-11 16:24:46 +0300

    package.json fix

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,36 +1,41 @@
 import askName from "../src/cli.js";
 import readlineSync from "readline-sync";
-import { checkCorrectAnswer, getRandomNumber } from "../src/index.js";
+import { checkCorrectAnswer } from "../src/index.js";
+import { getRandomNumber } from "../src/index.js";
 
 const generateProgression = (start, step, length) => {
-    return Array.from({ length }, (_, i) => start + i * step);
+  const progression = [];
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + i * step);
+  }
+  return progression;
 };
 
 function brainProgression() {
-    const name = askName();
-    console.log("What number is missing in the progression?");
+  const name = askName();
+  console.log("What number is missing in the progression?");
 
-    for (let i = 0; i < 3; i++) {
-        const start = getRandomNumber(1, 50);
-        const step = getRandomNumber(1, 10);
-        const length = getRandomNumber(5, 10);
-        const progression = generateProgression(start, step, length);
-        
-        const hiddenIndex = getRandomNumber(0, length - 1);
-        const correctAnswer = progression[hiddenIndex].toString();
+  let i = 0;
 
-        progression[hiddenIndex] = ".. ";
+  while (i < 3) {
+    const start = getRandomNumber();
+    const step = getRandomNumber(10);
+    const length = getRandomNumber(5, 10);
+    const progression = generateProgression(start, step, length);
+    const hiddenIndex = getRandomNumber(0, length - 1);
+    const correctAnswer = progression[hiddenIndex].toString();
 
-        console.log(`Question: ${progression.join(" ")}`);
-        const answer = readlineSync.question("Your answer: ");
+    progression[hiddenIndex] = "..";
+    console.log(`Question: ${progression.join(" ")}`);
 
-        if (checkCorrectAnswer(answer, correctAnswer, name)) {
-            continue;
-        } else {
-            return;
-        }
-    }
-    console.log(`Congratulations, ${name}!`);
+    const answer = readlineSync.question("Your answer: ");
+
+    const check = checkCorrectAnswer(answer, correctAnswer, name);
+
+    if (check) i += 1;
+    else return;
+  }
+  console.log(`Congratulations, ${name}!`);
 }
 
 brainProgression();
