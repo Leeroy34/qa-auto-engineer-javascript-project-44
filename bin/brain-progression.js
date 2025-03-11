158d7819a752452ec224b0d62824599a1acc2a9d
Даниил <you@example.com>
2025-03-11 16:11:58 +0300

    small fix

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -3,35 +3,35 @@ import readlineSync from "readline-sync";
 import { checkCorrectAnswer, getRandomNumber } from "../src/index.js";
 
 const generateProgression = (start, step, length) => {
-  return Array.from({ length }, (_, i) => start + i * step);
+    return Array.from({ length }, (_, i) => start + i * step);
 };
 
 function brainProgression() {
-  const name = askName();
-  console.log("What number is missing in the progression?");
+    const name = askName();
+    console.log("What number is missing in the progression?");
 
-  for (let i = 0; i < 3; i++) {
-    const start = getRandomNumber(1, 50);
-    const step = getRandomNumber(1, 10);
-    const length = Math.max(5, getRandomNumber(5, 10));
+    for (let i = 0; i < 3; i++) {
+        const start = getRandomNumber(1, 50);
+        const step = getRandomNumber(1, 10);
+        const length = getRandomNumber(5, 10);
+        const progression = generateProgression(start, step, length);
+        
+        const hiddenIndex = getRandomNumber(0, length - 1);
+        const correctAnswer = progression[hiddenIndex].toString();
 
-    const progression = generateProgression(start, step, length);
-    const hiddenIndex = getRandomNumber(0, length - 1);
-    const correctAnswer = progression[hiddenIndex].toString();
+        progression[hiddenIndex] = "..";
 
-    progression[hiddenIndex] = "..";
+        console.log(`Question: ${progression.join(" ")}`);
 
-    console.log(`Question: ${progression.join(" ")}`);
+        const answer = readlineSync.question("Your answer: ");
 
-    const answer = readlineSync.question("Your answer: ");
-
-    if (checkCorrectAnswer(answer, correctAnswer, name)) {
-      continue;
-    } else {
-      return;
+        if (checkCorrectAnswer(answer, correctAnswer, name)) {
+            continue;
+        } else {
+            return;
+        }
     }
-  }
-  console.log(`Congratulations, ${name}!`);
+    console.log(`Congratulations, ${name}!`);
 }
 
 brainProgression();
